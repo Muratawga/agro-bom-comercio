63f18d69c9d2f6497b6ccec162a2b5b74f473768
Vitor <vi.souza.almeida@protonmail.com>
2021-09-07 16:18:29 -0300

    responsividade do footer e dos iframes

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-<footer class="text-center">
+<footer class="text-center bg-green">
     <!-- Copyright -->
-        <div class="container">
+        <div class="container pt-2">
             <div class="row">
                 <div class="col ml-auto">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScSHfKeyEFbSV-MqgOWGvCdzUKT1POQzeQnu_Av74LF_fR4VQ/viewform?embedded=true" width="400" height="200" frameborder="0" >Carregando…</iframe>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScSHfKeyEFbSV-MqgOWGvCdzUKT1POQzeQnu_Av74LF_fR4VQ/viewform?embedded=true"  height="200" frameborder="0" >Carregando…</iframe>
                 </div>
                 <div class="col">
-                    
+                    <p class="text-light h5">Desenvolvido e entrege por <em><a href="https://phogtech.vercel.app/" target="_blank" rel="noopener noreferrer" class="text-warning font-weight-bold">PhogTech</a></em></p>
+                    <p class="text-light pt-5 mt-5 h5">
+                        Todos os direitos reservados
+                    </p>
                 </div>
                 <div class="col mr-auto">
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc3AsLwSN_-y6zErWQkdtg03FZ04-QkcYphP8-_eWgdq2tECA/viewform?embedded=true" width="400" height="200" frameborder="0" >Carregando…</iframe>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc3AsLwSN_-y6zErWQkdtg03FZ04-QkcYphP8-_eWgdq2tECA/viewform?embedded=true"  height="200" frameborder="0" >Carregando…</iframe>
                 </div>
               </div>
             </div>
