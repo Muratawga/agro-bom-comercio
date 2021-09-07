a19c3a7f193d5b4fee7516d65f6b1499e1a73d64
Vitor <vi.souza.almeida@protonmail.com>
2021-09-07 13:38:18 -0300

    feat: adição de forms Google no Footer

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
-<footer class="bg-light text-center text-lg-start">
+<footer class="text-center">
     <!-- Copyright -->
-    <div class="text-center text-light p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2021 Copyright:
-        <a class="text-light" href="https://phogtech.vercel.app">phogtech. </a>
-    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col ml-auto">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScSHfKeyEFbSV-MqgOWGvCdzUKT1POQzeQnu_Av74LF_fR4VQ/viewform?embedded=true" width="400" height="200" frameborder="0" >Carregando…</iframe>
+                </div>
+                <div class="col">
+                    
+                </div>
+                <div class="col mr-auto">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc3AsLwSN_-y6zErWQkdtg03FZ04-QkcYphP8-_eWgdq2tECA/viewform?embedded=true" width="400" height="200" frameborder="0" >Carregando…</iframe>
+                </div>
+              </div>
+            </div>
+        </div>
     <!-- Copyright -->
 </footer>
 
